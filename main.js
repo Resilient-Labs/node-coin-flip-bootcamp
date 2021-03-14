722632c577e64114354009ff71f4640e61613aa4
tamika sterlin <sterlintamika@gmail.com>
2021-03-14 17:32:38 -0400

    update readme

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-
-
 document.querySelector('.head').addEventListener('click', ifHeads)
-
 document.querySelector('.tail').addEventListener('click', ifTails)
 
 
 let heads = 0
 let tails = 0
+
 const front = document.createElement('img')
+front.classList.add('coin-img')
+document.getElementById('coin').appendChild(front)
 front.src = 'headss.png'
-const back = document.createElement('img')
-back.src = 'tailss.png'
 
-document.getElementByClass('coin').appendChild(front)
-document.getElementById('back').classList.add('hide')
-
-
-
-function ifHeads(){
- if(Math.floor(Math.random()*2) == 0){
-  document.querySelector('.result').innerText = 'You win!'
-} else{
-  document.querySelector('.result').innerText = 'Oops you lose :('
- }
+function ifHeads() {
+  if (Math.floor(Math.random() * 2) == 0) {
+    document.querySelector('.coin-img').src = 'headss.png'
+    document.querySelector('.display').innerText = 'You win!'
+  } else {
+    document.querySelector('.coin-img').src = 'tailss.png'
+    document.querySelector('.display').innerText = 'Oops you lose :('
+  }
 }
 
-function ifTails(){
- if(Math.floor(Math.random()*2) == 1){
-  document.querySelector('.result').innerText = 'You Win :)'
+function ifTails() {
+  if (Math.floor(Math.random() * 2) == 1) {
+    document.querySelector('.coin-img').src = 'tailss.png'
+    document.querySelector('.display').innerText = 'You Win :)'
   } else {
-  document.querySelector('.result').innerText = 'Oops you lose :('
+    document.querySelector('.coin-img').src = 'headss.png'
+    document.querySelector('.display').innerText = 'Oops you lose :('
   }
 }
--- server.js
+++ server.js
@@ -22,6 +22,27 @@ const server = http.createServer(function(req, res) {
       res.write(data);
       res.end();
     });
+  } else if (page == '/style.css') {
+    fs.readFile('style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  } else if(page == '/headss.png') {
+    fs.readFile('headss.png', function(err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'image/png'
+      });
+      res.write(data);
+      res.end();
+    });
+  } else if(page == '/tailss.png') {
+    fs.readFile('tailss.png', function(err, data) {
+      res.writeHead(200, {
+        'Content-Type': 'image/png'
+      });
+      res.write(data);
+      res.end();
+    });
   } else if (page == '/main.js') {
     fs.readFile('main.js', function(err, data) {
       res.writeHead(200, {
