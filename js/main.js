8079c33c645ca0feb5cd7d49bf40158e9017df8e
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-03-15 03:49:17 -0400

    Node Coin Flip
    I completed: 5
    I feel good: 4

--- js/main.js
+++ js/main.js
@@ -2,29 +2,17 @@
 //make probably for the coin to flip-- use math.random and math.ceil to round it to a whole number
 //client side
 
-document.querySelector('#flipCoin').addEventListener('click', fetchingCoin)
-
-function fetchingCoin(){
-    fetch ('/api')
-    .then(function (res){
-        return res.json() 
-    })
-    .then(data=>{
-        console.log(data.answer)
-    })
-}
-
 document.querySelector('#flipCoin').addEventListener('click', coinFlips)
 
 function coinFlips() {
     let probability=Math.ceil(Math.random()*2)
     if (probability%2===0){
-        document.querySelector('img').src='../img/head.png'
-        document.querySelector('span').innerHTML='Head Wins'
+        document.querySelector('img').src='img/head.png'
+        document.querySelector('p').innerHTML='Head Wins'
         console.log('head')
     }else{
-        document.querySelector('img').src='../img/tail.png'
-        document.querySelector('span').innerHTML='Tail Wins'
+        document.querySelector('img').src='img/tail.png'
+        document.querySelector('p').innerHTML='Tail Wins'
         console.log('tail')
     }
 }
