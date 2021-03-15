5b35a0c862205978fc828b69f250dc4bedb429e2
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-03-15 03:52:26 -0400

    Node Coin Flip Answer
    I completed the challenge: 5
    I feel good about my code:4

--- js/main.js
+++ js/main.js
@@ -2,17 +2,41 @@
 //make probably for the coin to flip-- use math.random and math.ceil to round it to a whole number
 //client side
 
+<<<<<<< Updated upstream
+=======
+document.querySelector('#flipCoin').addEventListener('click', fetchingCoin)
+
+function fetchingCoin(){
+    fetch ('/api')
+    .then(function (res){
+        return res.json() 
+    })
+    .then(data=>{
+        console.log(data.answer)
+    })
+}
+
+>>>>>>> Stashed changes
 document.querySelector('#flipCoin').addEventListener('click', coinFlips)
 
 function coinFlips() {
     let probability=Math.ceil(Math.random()*2)
     if (probability%2===0){
+<<<<<<< Updated upstream
         document.querySelector('img').src='img/head.png'
         document.querySelector('p').innerHTML='Head Wins'
         console.log('head')
     }else{
         document.querySelector('img').src='img/tail.png'
         document.querySelector('p').innerHTML='Tail Wins'
+=======
+        document.querySelector('img').src='../img/head.png'
+        document.querySelector('span').innerHTML='Head Wins'
+        console.log('head')
+    }else{
+        document.querySelector('img').src='../img/tail.png'
+        document.querySelector('span').innerHTML='Tail Wins'
+>>>>>>> Stashed changes
         console.log('tail')
     }
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 //nodeJS works on a terminal interpret where it interpret JS
 //nodeJS -- helps differentiate difference between webpage and browser
 //browser and JS can work independent 
+<<<<<<< Updated upstream
 //
 
 //require=requesting whatever inside para file
@@ -26,3 +27,70 @@ http.createServer(function (req, res) {
     res.end();
   });
 }).listen(8000);
+=======
+
+//require= requesting whatever inside para file
+//http-set up set webserver
+//fs- access file system (can be a whole comp--whatever we tell it)
+
+//helped by Jeffrey and Kathy 
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    //connecting server side to 
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+  else if (page == '/api') {
+      let result 
+      let probability=Math.ceil(Math.random()*2)
+      if (probability%2===0){
+        result='Head'
+      }else{
+        result='Tail'
+      }
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          answer: result
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+  //connect to server css file
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data)
+      res.end()
+    })
+    //connecting server to main.js file
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    })
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
+>>>>>>> Stashed changes
