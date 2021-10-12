193a139de2ce55811cf8bc29d9b7ba1674fdf737
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-10-12 02:19:32 -0400

    updateCoinFlip

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const querystring = require("querystring");
+const figlet = require("figlet");
+
+const server = http.createServer(function (req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == "/") {
+    fs.readFile("index.html", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  } else if (page == "/api") {
+    if ("coin" in params) {
+      let bot = Math.floor(Math.random() * 2) == 1 ? "Tails" : "Heads";
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const objToJson = {
+        botChoice: bot,
+        humanVsbot:
+          params["coin"] === bot
+            ? `The coin flipped ${bot}. You win!`
+            : `The coin flipped ${bot}. Sorry, you lost.`,
+      };
+
+      
+
+      res.end(JSON.stringify(objToJson));
+    }
+  } else if (page == "/css/style.css") {
+    fs.readFile("css/style.css", function (err, data) {
+      res.write(data);
+      res.end();
+    });
+  } else if (page == "/js/main.js") {
+    fs.readFile("js/main.js", function (err, data) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    figlet("404!!", function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8001);
