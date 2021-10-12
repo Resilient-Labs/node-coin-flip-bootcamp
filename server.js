c9ab997ddd9915f69e1bf84f3c053bdffd6b3caf
Alexa Macias <Alexa Macias>
2021-10-11 21:35:21 -0400

    answer

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    if('coin' in params){
+      let bot= Math.floor(Math.random()*2) == 1 ? 'tails' : 'heads'
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          botChoice: bot,
+          humanVsbot: params['coin'] === bot ? `the coin result is ${bot} you win`:`the coin result is ${bot} you lose`,     
+      }
+        
+      res.end(JSON.stringify(objToJson));
+    }//student if
+  }//else if
+  
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
