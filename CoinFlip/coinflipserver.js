0b493819301326a4e42210860b22cd5ed0b9c31b
Tyler <tyler.robinson.business@gmail.com>
2021-10-12 23:11:52 -0400

    thirdcommit

--- CoinFlip/coinflipserver.js
+++ CoinFlip/coinflipserver.js
@@ -1,25 +1,43 @@
-const http = require('http')
+const http = require('http');
 const fs = require('fs')
-const url = require('url')
-const port = 2500
-
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
 
 const server = http.createServer(function(req, res) {
-    const page = url.parse(req.url).pathname;
-    if (page == '/coinflip.html') {
-        fs.readFile('coinflip.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data)
-        console.log("Working")
-        })
-    }
-    res.end();
-    })
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('coinflip.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+//   else if (page == '/css/style.css'){
+//     fs.readFile('css/style.css', function(err, data) {
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+  else if (page == '/js/coinscript.js'){
+    fs.readFile('js/coinscript.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log("Something went wrong")
-    } else {
-        console.log(`Server is up on port: ${port}, open browser.`)
-    }
-})
+server.listen(8000);
--- CoinFlip/js/coinscript.js
+++ CoinFlip/js/coinscript.js
@@ -2,7 +2,7 @@ document.querySelector("#button").addEventListener('click', tossCoin)
 let picsource = document.getElementById("flipMe")
 
 function tossCoin(){
-    let pic = new Array("coinfront.png", "coinback.png");
+    let pic = new Array("https://i.imgur.com/d0Wiv32.png", "https://i.imgur.com/vwHTlIQ.png");
     let randompics = Math.floor(Math.random() * pic.length);
     picsource.src = pic[randompics];
 }
