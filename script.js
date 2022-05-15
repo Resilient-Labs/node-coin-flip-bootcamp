bd25b881ce517ae05813864e6986eba3cccff9b0
Julian Rosa <rosajulian1930@gmail.com>
2022-05-15 19:57:29 -0400

    Node Coin Flip

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', headsOrTails))
+let playerScore = 0
+let compScore = 0
+
+function headsOrTails(e) {
+
+    let playerSelected = e.target.classList;
+    document.querySelector('.playerSelected').innerText = playerSelected
+    
+    fetch(`/api?playerChoice=${playerSelected}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if(data.playerWin == true){
+            document.querySelector('.playerScore').innerText = playerScore           
+            playerScore++
+        } else{
+            compScore++
+            document.querySelector('.compScore').innerText = compScore
+        }
+        document.querySelector('.coinImg').innerHTML = `<img src='imgs/${data.coinFlip}.png'>`
+        });
+
+}
