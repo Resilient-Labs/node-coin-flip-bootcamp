b83fc3e81c0f8e867c66fd7243ba150cffa986a2
Leo Constant <101950707+leonardconstant1995@users.noreply.github.com>
2022-05-17 16:10:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ const results = document.querySelector('h2')
 const head = document.querySelector('#head')
 const tail = document.querySelector('#tail')
 
-console.log(option)
 
 button.addEventListener('click', coinFlip)
 
 function coinFlip() {
+
     let coinLand = option.value
-    let probability = Math.ceil(Math.random() * 2)
-    if( probability === 2 && coinLand === 'tails') {
-        results.innerText = 'Heads! Your Wrong!'
-    } else if ( probability === 1 && coinLand === 'heads') {
-        results.innerText = 'Tails! Nope!'
-    } else if ( probability === 2 && coinLand === 'heads') {     
-        results.innerText = 'Heads! Right On!'
-    } else if ( probability === 1 && coinLand === 'tails') {       
-        results.innerText = 'Tails! Nice!'
-    }
+
+    fetch(`/api?cointoss=${coinLand}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+    console.log(data)
+
+        results.innerText = `${data.result}`
+    })
+    .catch(err => {
+    console.log(`error ${err}`)
+    });
+    
 }
