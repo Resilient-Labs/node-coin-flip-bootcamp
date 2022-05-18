d4e525b9c42c65d44891275e947c6f615b34b7b1
Travora Senecharles <travora9@gmail.com>
2022-05-18 03:16:02 -0400

    I got my picture to load and the site is simple but cute, I think.

--- server.js
+++ server.js
@@ -5,70 +5,82 @@ const querystring = require('querystring');// is the paramater of the url
 const figlet = require('figlet')
 
 
- const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
   if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
+
+
+
   } else if (page == '/api') { // this is what we will use for the fetch call
-    if('bet' in params){
-        let tails = 1
-        let heads = 0
-        let flip = Math.floor(Math.random () * 2)
-      if(params['bet']== 'heads'&& flip === 0 ){
-
-        res.writeHead(200, {'Content-Type': 'application/json'});
+    if ('bet' in params) {
+      let tails = 1
+      let heads = 0
+      let flip = Math.floor(Math.random() * 2)
+      if (params['bet'] == 'Heads' && flip === 0) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const resultForServer = {
-          result: "you won",
-          
+          result: "Heads up! You won.",
         }
         res.end(JSON.stringify(resultForServer));
       }
-      else if(params['bet']== 'heads'&& flip === 1 ){
+      else if (params['bet'] == 'Heads' && flip === 1) {
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const resultForServer = {
-          result: "you lost",
-          
+          result: "Head out - you lost.",
+
         }
         res.end(JSON.stringify(resultForServer));
       }
-      else if(params['bet']== 'tails'&& flip === 0 ){
+      else if (params['bet'] == 'Tails' && flip === 0) {
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const resultForServer = {
-          result: "you lost",
-          
+          result: "The CPU was ahead of the pack...you were the tail-end.",
         }
         res.end(JSON.stringify(resultForServer));
       }
-      else if(params['bet']== 'tails'&& flip === 1 ){
+      else if (params['bet'] == 'Tails' && flip === 1) {
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const resultForServer = {
-          result: "you won",
-          
+          result: "A hero's tail - you won!",
+
         }
         res.end(JSON.stringify(resultForServer));
       }
     }
-  } else if (page == '/main.js'){
-    fs.readFile('/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
+  } else if (page == '/main.js') {
+    fs.readFile('main.js', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
       res.write(data);
       res.end();
     });
-  }else{
-    figlet('404!!', function(err, data) {
+  } else if (page == '/flipPic.gif') {
+    fs.readFile('flipPic.gif', function (err, data) {
+      res.writeHead(200), { 'Content-Type': 'image/gif' }
+      res.write(data)
+      res.end()
+    })
+  } else if (page == '/style.css') {
+    fs.readFile('style.css', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    figlet('404!!', function (err, data) {
       if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
       }
       res.write(data);
       res.end();
