c4dc68d5264e03a148692bf12d50ab04d8c8971f
Tashan Phillips <tashanphillips@Tashans-MacBook-Pro.local>
2022-10-23 22:32:07 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,93 @@
+//worked with leny, zain, victor
+
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    flipArray = ['heads', 'tails']
+    if('student' in params){
+      if(params['student'] == 'heads'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        randomFlip = flipArray[Math.floor(Math.random() * flipArray.length)]
+        if(params['student'] === randomFlip) {
+          winOrLoss = 'Won'
+        }else{
+          winOrLoss = "Lost"
+        }
+        const objToJson = {
+          personChoice: "You chose heads",
+          flipResult: `The flip was ${randomFlip}`,
+          personWinOrLoss: `You ${winOrLoss}`
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student = leon
+      else if(params['student'] == 'tails'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        randomFlip = flipArray[Math.floor(Math.random() * flipArray.length)]
+        if(params['student'] === randomFlip) {
+          winOrLoss = 'Won'
+        }else{
+          winOrLoss = "Lost"
+        }
+        const objToJson = {
+          personChoice: "You chose tails",
+          flipResult: `The flip was ${randomFlip}`,
+          personWinOrLoss: `You ${winOrLoss}`
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student != leon
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
