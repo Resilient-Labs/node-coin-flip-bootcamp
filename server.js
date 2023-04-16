8e1c52f3a4bdb0a6eb96a0a16977c0c397b672ee
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-04-16 01:40:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const PORT = 8000
+
+const server = http.createServer(function(req, res) {
+    const page = url.parse(req.url).pathname;
+    const params = querystring.parse(url.parse(req.url).query);
+    console.log(page);
+    if (page == '/') {
+        fs.readFile('index.html', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+        });
+      }else if (page == '/css/style.css'){
+        fs.readFile('css/style.css', function(err, data) {
+          res.write(data);
+          res.end();
+        });
+      }else if (page == '/js/main.js'){
+        fs.readFile('js/main.js', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }else{
+        console.log('Something went wrong...');
+      }
+
+})
+server.listen(PORT);
