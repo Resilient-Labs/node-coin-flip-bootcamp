a8c94f34148a09fabcf850a97997a0fd92a7f5b8
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-16 15:34:48 -0400

    Needs CSS

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-// const headInput = document.querySelector('#head')
-// const tailInput = document.querySelector('#tail')
-// const button = document.querySelector('#button')
-// const display = document.querySelector('h2')
-
-// function playGame(){
-//   return Math.random()< .5 ? "heads" : "tails" ;
-// }
-
-// function userInput(e){
-//   e.preventDefault()
-
-//   const headInput = document.querySelector('#head').value
-//   const tailInput = document.querySelector('#tail').value
-
-//   if (headInput === playGame() || tailInput === playGame()){
-//     document.querySelector('h2').innerText= 'You Win!'
-//   } else{
-//     document.querySelector('h2').innerText= 'Try Again!'
-//   }
-// }
-//document.querySelector('#submit').addEventListener('click', userInput)
 function sendReq(){
 
   const coin = document.querySelector('input[name="game"]:checked').value
@@ -32,7 +10,7 @@ function sendReq(){
     document.querySelector('#placeToSee').textContent = data.winOrLoose
   }) 
 
-   console.log(coin)
+  //  console.log(coin)
 }
 
 document.querySelector('#submit').addEventListener('click', sendReq)
