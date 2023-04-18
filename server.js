a29054d1bc3a00672b1c83192ca03b4b76826584
RESILIENTOMAR <TEIXEIRAOMAR1@GMAIL.COM>
2023-04-17 21:19:24 -0400

    second push attempt

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function (req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/random') {
+    const randomNum = Math.floor(Math.random() * 2)
+    const coin = randomNum ? "heads" : "tails"
+    const guess = params['guess'].toLowerCase().trim()
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const objToJson = {
+      random: coin,
+      winOrLoss: coin === guess ? "You Win !" : "You Lose !"
+    }
+    res.end(JSON.stringify(objToJson));
+  }
+
+  else if (page == '/css/style.css') {
+    fs.readFile('css/style.css', function (err, data) {
+      res.write(data);
+      res.end();
+    });
+  } else if (page == '/js/main.js') {
+    fs.readFile('js/main.js', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    figlet('404!!', function (err, data) {
+      if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
