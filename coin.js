c10ab390a0ae8f96207cff09f8845f4fedda271f
Khadir | FsDevKD <125905976+FSDevKD@users.noreply.github.com>
2023-04-29 11:45:50 -0400

    pushing

--- coin.js
+++ coin.js
@@ -1,29 +1,9 @@
 const http = require('http');
 const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        fs.readFile('./coin.html', (err, data) => {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        });
-    }
-});
-
-server.listen(3000, () => {
-    console.log('listening on port 3000');
-});
-
-function guessCoin(){
-    const guess = document.getElementById('guess');
-    const result = document.getElementById('result');
-    const coin = Math.random() < 0.5 ? 'heads' | 'tails';
-
-    if(guess.value === coin) {
-        result.textContent = `You guessed correctly! Way To Go!! ${guess} was right`
-    } else {
-        result.textContent = `You guessed ${guess} but the coin was not correct! it was ${coin}`;
-    }
-    guess.value = '';
-}
+http.createServer(function (req, res) {
+  fs.readFile('demofile.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(3000);
